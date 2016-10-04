93ca99ee1e30e810f6117dc4fd1986532529531e
SPAND <SPAND@SPAND-PC>
2016-10-04 09:47:44 +0200

    added testmethods for all 8 winning posibilities, and the methods to solve them

--- GameWinnerService/GameWinnerService.cs
+++ GameWinnerService/GameWinnerService.cs
@@ -15,19 +15,46 @@ namespace TicTacToe.Services
         private const char SymbolForNoWinner = ' ';
         public char Validate(char[,] gameBoard)
         {
-            var currentWinningSymbol = CheckForThreeInARowInHorizontalRow(gameBoard);
+            // HORIZONTAL CHECKS
+            var currentWinningSymbol = CheckForThreeInARowInTopHorizontalRow(gameBoard);
             if (currentWinningSymbol != SymbolForNoWinner) 
                 return currentWinningSymbol;
-            currentWinningSymbol = CheckForThreeInARowinVerticalColumn(gameBoard);
+            // new horizontalrow checks
+            currentWinningSymbol = CheckForThreeInARowInMiddleHorizontalRow(gameBoard);
             if (currentWinningSymbol != SymbolForNoWinner)
                 return currentWinningSymbol;
+            currentWinningSymbol = CheckForThreeInARowInBottomHorizontalRow(gameBoard);
+            if (currentWinningSymbol != SymbolForNoWinner)
+                return currentWinningSymbol;
+
+            // VERTICAL CHECKS
+            currentWinningSymbol = CheckForThreeInARowInTopVerticalColumn(gameBoard);
+            if (currentWinningSymbol != SymbolForNoWinner)
+                return currentWinningSymbol;
+            // new vertical checks
+            currentWinningSymbol = CheckForThreeInARowInMiddleVerticalColumn(gameBoard);
+            if (currentWinningSymbol != SymbolForNoWinner)
+                return currentWinningSymbol;
+            currentWinningSymbol = CheckForThreeInARowInBottomVerticalColumn(gameBoard);
+            if (currentWinningSymbol != SymbolForNoWinner)
+                return currentWinningSymbol;
+
+            // DIAGONAL CHECKS
             currentWinningSymbol = CheackForThreeInARowDiagonallyDown(gameBoard);
             if (currentWinningSymbol != SymbolForNoWinner)
                 return currentWinningSymbol;
+            // new diagonal up and right
             currentWinningSymbol = CheackForThreeInARowDiagonallyUp(gameBoard);
             return currentWinningSymbol;
         }
-            private static char CheckForThreeInARowinVerticalColumn(char[,] gameBoard)
+
+        /*
+           Navneændring fra
+           CheckForThreeInARowinVerticalColumn (det andet "in" startede også med småt før)
+           til
+           CheckForThreeInARowInTopVerticalColumn
+         */
+        private static char CheckForThreeInARowInTopVerticalColumn(char[,] gameBoard)
         {
             var rowOneChar = gameBoard[0, 0];
             var rowTwoChar = gameBoard[1, 0];
@@ -38,12 +65,65 @@ namespace TicTacToe.Services
             }
             return SymbolForNoWinner;
         }
-
-        private static char CheckForThreeInARowInHorizontalRow(char[,] gameBoard)
+        // mid row set
+        private static char CheckForThreeInARowInMiddleVerticalColumn(char[,] gameBoard)
+        {
+            var rowOneChar = gameBoard[0, 1];
+            var rowTwoChar = gameBoard[1, 1];
+            var rowThreeChar = gameBoard[2, 1];
+            if (rowOneChar == rowTwoChar && rowTwoChar == rowThreeChar)
+            {
+                return rowOneChar;
+            }
+            return SymbolForNoWinner;
+        }
+        // bottom row set
+        private static char CheckForThreeInARowInBottomVerticalColumn(char[,] gameBoard)
+        {
+            var rowOneChar = gameBoard[0, 2];
+            var rowTwoChar = gameBoard[1, 2];
+            var rowThreeChar = gameBoard[2, 2];
+            if (rowOneChar == rowTwoChar && rowTwoChar == rowThreeChar)
+            {
+                return rowOneChar;
+            }
+            return SymbolForNoWinner;
+        }
+        /*
+          navneændring fra
+          CheckForThreeInARowInHorizontalRow
+          til
+          CheckForThreeInARowInTopHorizontalRow
+        */
+        private static char CheckForThreeInARowInTopHorizontalRow(char[,] gameBoard)
         {
             var columnOneChar = gameBoard[0, 0];
             var columnTwoChar = gameBoard[0, 1];
             var columnThreeChar = gameBoard[0, 2];
+            if (columnOneChar == columnTwoChar && columnTwoChar == columnThreeChar)
+            {
+                return columnOneChar;
+            }
+            return SymbolForNoWinner;
+        }
+        // to nye HorizontalRow methods
+        private static char CheckForThreeInARowInMiddleHorizontalRow(char[,] gameBoard)
+        {
+            var columnOneChar = gameBoard[1, 0];
+            var columnTwoChar = gameBoard[1, 1];
+            var columnThreeChar = gameBoard[1, 2];
+            if (columnOneChar == columnTwoChar && columnTwoChar == columnThreeChar)
+            {
+                return columnOneChar;
+            }
+            return SymbolForNoWinner;
+        }
+
+        private static char CheckForThreeInARowInBottomHorizontalRow(char[,] gameBoard)
+        {
+            var columnOneChar = gameBoard[2, 0];
+            var columnTwoChar = gameBoard[2, 1];
+            var columnThreeChar = gameBoard[2, 2];
             if (columnOneChar == columnTwoChar && columnTwoChar == columnThreeChar)
             {
                 return columnOneChar;
@@ -62,6 +142,7 @@ namespace TicTacToe.Services
             }
             return SymbolForNoWinner;
         }
+        // new
         private static char CheackForThreeInARowDiagonallyUp(char[,] gameBoard)
         {
             var cellOneChar = gameBoard[2, 0];
--- TicTacToe.Tests/GameWinnerServiceTests.cs
+++ TicTacToe.Tests/GameWinnerServiceTests.cs
@@ -70,13 +70,78 @@ namespace TicTacToe.Tests
             Assert.AreEqual(expected.ToString(), actual.ToString());
         }
 
+        //new tests from here on down
+
+        // anden kolonne
+        [TestMethod] 
+        public void PlayerWithAllSpacesInSecondColumnIsWinner()
+        {
+            const char expected = 'X';
+            for (var columnIndex = 0; columnIndex < 3; columnIndex++)
+            {
+                //for loop sets [0,1],[1,1],[2,1]
+                gameBoard[columnIndex, 1] = expected;
+            }
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        // tredje kolonne
+        [TestMethod]
+        public void PlayerWithAllSpacesInThirdColumnIsWinner()
+        {
+            const char expected = 'X';
+            for (var columnIndex = 0; columnIndex < 3; columnIndex++)
+            {
+                //for loop sets [0,2],[1,2],[2,2]
+                gameBoard[columnIndex, 1] = expected;
+            }
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        // anden række
+        [TestMethod]
+        public void PlayerWithAllSpacesInMiddleRowIsWinner()
+        {
+            const char expected = 'X';
+            for (var columnIndex = 0; columnIndex < 3; columnIndex++)
+            {
+                //for loop sets [1,0],[1,1],[1,2]
+                gameBoard[1, columnIndex] = expected;
+            }
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        // tredje række
+        [TestMethod]
+        public void PlayerWithAllSpacesInBottomRowIsWinner()
+        {
+            const char expected = 'X';
+            for (var columnIndex = 0; columnIndex < 3; columnIndex++)
+            {
+                //for loop sets [2,0],[2,1],[2,2]
+                gameBoard[2, columnIndex] = expected;
+            }
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        /*
+            Skråt op til højre
+            [' '],[' '],['X']
+            [' '],['X'],[' ']
+            ['X'],[' '],[' ']
+        */
         [TestMethod]
         public void PlayerWithThreeInARowDiagonallyUpAndToRightIsWinner()
         {
+
             const char expected = 'X';
             int columnIndex = 2;
             for (var rowIndex = 0; rowIndex < 3; rowIndex++)
-            {
+            {   //for loop checks [2,0],[1,1],[0,2]
                 gameBoard[columnIndex, rowIndex] = expected;
                 columnIndex--;
             }
